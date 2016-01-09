4ad3650e42468326035e7a5853779aef58800ed2
tinykidtoo <tinykidtoodotcom@yahoo.com>
2016-01-09 13:57:56 -0800

    Jan 9th 2pm
    its a game or atleast a bad rts

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,30 +2,45 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour
+{
 	public float amourClass = 13f;
 	[SerializeField] private float myHealth = 100f;
 	[SerializeField] private float myMaxHealth = 100f;
 	[SerializeField] private Slider hpSlider;
+	[SerializeField] private GameObject deathPrefab;
+	private bool dead = false;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		myHealth = myMaxHealth;
 		hpSlider.maxValue = myHealth;
 	}
 
-	public void ApplyDamage (float dmg) {
+	public void ApplyDamage (float dmg)
+	{
 		myHealth -= dmg;
 		CheckHealth ();
 	}
 
-	void CheckHealth () {
+	void CheckHealth ()
+	{
 		hpSlider.value = myHealth;
 		if (myHealth <= 0) {
 			myHealth = 0;
 			Debug.Log (gameObject.name + " has died");
 			Destroy (gameObject);
+			if (this.tag == "Player")
+				SinmpleWinCon.Players--;
+			if (this.tag == "Enemy")
+				SinmpleWinCon.enemies--;
+			if (!dead) {
+				Instantiate (deathPrefab, transform.position, transform.rotation);
+				dead = true;
+				Destroy (gameObject);
+			}
 		}
-		if(myHealth >= myMaxHealth) {
+		if (myHealth >= myMaxHealth) {
 			myHealth = myMaxHealth;
 		}
 	}
---
+++ Assets/Scripts/SinmpleWinCon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SinmpleWinCon : MonoBehaviour {
+	public static int Players = 0;
+	public Text youLose;
+	public static int enemies = 0;
+	public Text youWin;
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] gos1 = GameObject.FindGameObjectsWithTag ("Player");
+		Players = gos1.Length;
+		GameObject[] gos = GameObject.FindGameObjectsWithTag ("Enemy");
+		enemies = gos.Length;
+		youWin.enabled = false;
+		youLose.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Players <= 0)
+			youLose.enabled = true;
+		if (enemies <= 0)
+			youWin.enabled = true;
+	}
+}
